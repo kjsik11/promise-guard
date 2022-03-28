af7c464cf32f06fa798185f736e652967ccac657
jongsik <kjsik11@gmail.com>
2022-03-28 22:56:20 +0900

    fix: minor fix

--- src/frontend/components/layout/Common.tsx
+++ src/frontend/components/layout/Common.tsx
@@ -1,12 +1,11 @@
 import Footer from '@frontend/components/core/Footer';
 import Header from '@frontend/components/core/Header';
-import Countdown from '@frontend/components/custom/Countdown';
 
 export default function CommonLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="relative mx-auto h-full max-w-3xl">
       <Header />
-      <Countdown />
+
       {children}
       <Footer />
     </main>
