4a6f3af692924c150080b22de040a6236f0bb62b
jongsik <kjsik11@gmail.com>
2022-04-05 17:04:11 +0900

    feat: Add pretendard font on project
    - add theme colors on tailwind config
    - minor fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,15 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        // 'Inter' fontFamily was imported in _document.tsx
-        inter: "'Inter'",
+        pretendard:
+          "Pretendard, -apple-system, 'system-ui', system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', arial, sans-serif;",
+      },
+      colors: {
+        'PC-200': '#609BFF',
+        'PC-400': '#6A99CD',
+        'PC-600': '#285A92',
+        'PC-800': '#082E59',
+        KAKAO: '#FEE5000',
       },
     },
   },
