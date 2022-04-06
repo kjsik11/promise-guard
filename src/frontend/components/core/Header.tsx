f5a5e9e7ced1bf386d71be6569a7beb0204113d3
jongsik <kjsik11@gmail.com>
2022-04-06 16:18:13 +0900

    fix: update header

--- src/frontend/components/core/Header.tsx
+++ src/frontend/components/core/Header.tsx
@@ -1,6 +1,5 @@
 import { Menu, Transition } from '@headlessui/react';
 import { MenuIcon } from '@heroicons/react/outline';
-import clsx from 'clsx';
 import NextLink from 'next/link';
 import { Fragment } from 'react';
 
@@ -8,10 +7,8 @@ import Logo from '@frontend/components/vector/Logo';
 import useUser from '@frontend/hooks/use-user';
 
 const menuItems = [
-  { label: '프로젝트 소개', href: '#' },
-  { label: '의견 제보', href: '#' },
-  { label: '공약 출처', href: '#' },
-  { label: '공약 정정·추가 요청', href: '#' },
+  { label: '오월,십일 소개', href: 'introduce' },
+  { label: '의견 제보 · 공약 수정 요청', href: '/feedback' },
 ];
 
 export default function Header() {
@@ -41,37 +38,36 @@ export default function Header() {
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95"
           >
-            <Menu.Items className="absolute right-0 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Menu.Items className="absolute right-0 mt-2 w-[200px] origin-top-right divide-y divide-gray-100 rounded-md bg-white text-gray-700 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
               <nav className="py-1">
                 {menuItems.map((item, idx) => (
                   <Menu.Item key={`nav-item-${item.label}-${idx}`}>
                     <NextLink href={item.href}>
-                      <a
-                        className={clsx(
-                          'flex w-full items-center px-4 py-2 transition-colors hover:bg-violet-400',
-                          {
-                            'mt-1 border-t border-gray-200': item.label === '로그인',
-                          },
-                        )}
-                      >
-                        {item.label}
-                      </a>
+                      <a className="flex w-full items-center px-4 py-2">{item.label}</a>
                     </NextLink>
                   </Menu.Item>
                 ))}
               </nav>
-
-              <Menu.Item>
-                <button
-                  onClick={() => {
-                    if (user) handleSignout();
-                    else handleSignin();
-                  }}
-                  className="flex w-full items-center px-4 py-2 transition-colors hover:bg-violet-400"
-                >
-                  {user ? '로그아웃' : '로그인'}
-                </button>
-              </Menu.Item>
+              <div>
+                {user && (
+                  <Menu.Item>
+                    <NextLink href="/my-promise">
+                      <a className="flex w-full items-center px-4 py-2">내 공약</a>
+                    </NextLink>
+                  </Menu.Item>
+                )}
+                <Menu.Item>
+                  <button
+                    onClick={() => {
+                      if (user) handleSignout();
+                      else handleSignin();
+                    }}
+                    className="flex w-full items-center px-4 py-2 transition-colors"
+                  >
+                    {user ? '로그아웃' : '로그인'}
+                  </button>
+                </Menu.Item>
+              </div>
             </Menu.Items>
           </Transition>
         </Menu>
