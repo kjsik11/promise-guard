34b1f2a0ca0da6bbf1683f097800565dc47e3a22
jongsik <kjsik11@gmail.com>
2022-04-08 23:41:39 +0900

    fix: attatch kakao channel link

--- src/frontend/components/custom/KakaoButton.tsx
+++ src/frontend/components/custom/KakaoButton.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import Link from 'next/link';
 
 import { Kakao } from '@frontend/components/vector';
 
@@ -8,14 +9,18 @@ interface Props {
 
 export default function KakaoButton({ className }: Props) {
   return (
-    <button
-      className={clsx(
-        'flex w-full items-center  justify-center space-x-2 rounded-lg bg-Kakao py-2 px-4',
-        className,
-      )}
-    >
-      <Kakao />
-      <p className="font-semibold text-gray-900">카카오 채널 추가하기</p>
-    </button>
+    <Link href="http://pf.kakao.com/_kpfNb" passHref>
+      <a
+        target="_blank"
+        referrerPolicy="no-referrer"
+        className={clsx(
+          'mx-auto flex w-full max-w-sm items-center justify-center space-x-2 rounded-lg bg-Kakao py-2 px-4',
+          className,
+        )}
+      >
+        <Kakao />
+        <p className="font-semibold text-gray-900">카카오 채널 추가하기</p>
+      </a>
+    </Link>
   );
 }
