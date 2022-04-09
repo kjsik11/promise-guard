83a00f3b9d5c650c451449ba20aacc4921e3e464
jongsik <kjsik11@gmail.com>
2022-04-09 15:36:59 +0900

    fix: change countdown design

--- src/frontend/components/custom/Countdown.tsx
+++ src/frontend/components/custom/Countdown.tsx
@@ -40,9 +40,16 @@ const mainRenderer = ({ days, hours, minutes, seconds, completed }: CountdownPar
   // Render a countdown
   return (
     <div className="absolute top-full w-[280px] -translate-y-1/2 rounded-lg border border-gray-300 bg-white py-4 text-center">
-      <p className="text-sm font-semibold text-PC-400">투표마감 5월 10일 0시까지</p>
-      <p className="mx-auto w-[260px] text-2xl font-semibold text-PC-600">
-        {days}일 {hours}시간 {minutes}분 {seconds < 10 ? `0${seconds}` : seconds}초
+      <p className="text-sm font-semibold text-PC-400">투표마감 5월 10일 0시 까지</p>
+      <p className="mx-auto w-[260px] text-3xl font-bold text-PC-600">
+        <span>{days}</span>
+        <span className="pr-1.5 pl-0.5 text-xs">일</span>
+        <span>{hours < 10 ? `0${hours}` : hours}</span>
+        <span className="pr-1.5 pl-0.5 text-xs">시</span>
+        <span>{minutes}</span>
+        <span className="pr-1.5 pl-0.5 text-xs"> 분</span>
+        <span>{seconds < 10 ? `0${seconds}` : seconds}</span>
+        <span className="pr-1.5 pl-0.5 text-xs">초</span>
       </p>
     </div>
   );
