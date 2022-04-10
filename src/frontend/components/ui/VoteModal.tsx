2f52e247b07ee464b4c7a6eb344e50d81ce095fe
jongsik <kjsik11@gmail.com>
2022-04-10 20:53:32 +0900

    fix: update modal

--- src/frontend/components/ui/VoteModal.tsx
+++ src/frontend/components/ui/VoteModal.tsx
@@ -1,7 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react';
-import { XIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
-import Link from 'next/link';
 import { Fragment, useRef } from 'react';
 
 import voteImage from '@assets/images/vote-image.png';
@@ -40,41 +38,25 @@ export default function VoteModal({ show, close }: { show: boolean; close: () =>
             leaveFrom="opacity-100 translate-y-0"
             leaveTo="opacity-0 translate-y-4"
           >
-            <div className="absolute inset-x-0 w-full px-4 transition-all">
-              <div className="mx-auto max-w-sm rounded-lg bg-white px-4">
-                <div className="relative flex min-h-[527px] flex-col justify-between pt-12">
-                  <button onClick={close} className="absolute top-4 right-0">
-                    <XIcon className="h-6 w-6" />
+            <div className="relative z-10 mx-auto w-full max-w-sm rounded-lg bg-white px-4 py-6 transition-all">
+              <div className="relative flex min-h-[400px] flex-col">
+                <Image width={1280} height={900} src={voteImage} alt="vote-image" />
+                <div className="pt-8 text-center">
+                  <h3 className="text-2xl font-bold leading-8 text-gray-900" id="modal-headline">
+                    환영해요!
+                  </h3>
+                  <div className="mt-4">
+                    <p>
+                      대통령 취임일 5월 10일에 마감되는
+                      <br /> 대국민 공약 투표에 참여해주세요.
+                    </p>
+                  </div>
+                  <button
+                    onClick={close}
+                    className="mt-4 rounded-full bg-PC-400 py-1.5 px-3 text-sm font-semibold text-white"
+                  >
+                    시작하기
                   </button>
-                  <div className="text-center">
-                    <h3 className="text-2xl font-bold leading-8 text-gray-900" id="modal-headline">
-                      어떤 공약을 지지하시나요?
-                    </h3>
-                    <div className="mt-4">
-                      <p>
-                        카카오톡으로 3초만에 로그인해서
-                        <br /> 대국민 공약 투표에 참여할 수 있어요
-                      </p>
-                    </div>
-                    <div className="pt-4">
-                      <Link href="/introduce">
-                        <a className="rounded-full bg-PC-400 px-3 py-1.5 text-sm font-semibold text-white">
-                          프로젝트 소개
-                        </a>
-                      </Link>
-                      <div className="mt-3">
-                        <button onClick={close} className="text-sm font-semibold text-blue-500">
-                          닫기
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="relative -mb-[7px]">
-                    {/*eslint-disable-next-line */}
-                    <Image width={1280} height={900} src={voteImage} alt="vote-image" />
-                    <div className="absolute inset-0 top-0 h-8 bg-gradient-to-t from-white/0 to-white" />
-                  </div>
                 </div>
               </div>
             </div>
